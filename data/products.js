4a1adc8b07529fe3ca888265b4aa71e30d76b775
ThanhLong1601 <72308447+ThanhLong1601@users.noreply.github.com>
2024-09-26 11:26:31 +0700

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -59,6 +59,31 @@ class Clothing extends Product {
   }
 }
 
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
+// const ob2 = {
+//   a: 2,
+//   b: this.a
+// };
+
+// function logThis(){
+//   console.log(this);
+// }
+
+// logThis();
+// logThis.call('hello');
+
+// this
+// const ob3 = {
+//   method: () => {
+//     console.log(this);
+//   }
+// };
+// ob3.method();
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
