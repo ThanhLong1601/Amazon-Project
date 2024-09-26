114e923a48048f08346acfbd6882766be8b5875a
ThanhLong1601 <72308447+ThanhLong1601@users.noreply.github.com>
2024-09-26 13:03:12 +0700

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -97,7 +97,9 @@ export function loadProductsFetch(){
       return new Product(productDetails);
     });
     console.log('load products');
-  });
+  }).catch((error) => {
+    console.log("Unexpected error. Please try again later.");
+  })
   return promise;
 }
 // loadProductsFetch();
@@ -115,6 +117,10 @@ export function loadProducts(fun){
       console.log('load products');
       fun();
   });
+
+  xhr.addEventListener('error',(error) => {
+    console.log("Unexpected error. Please try again later.");
+  })
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send()
